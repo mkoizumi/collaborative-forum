c203c538b064c682108178ba80876512a3d9be6c
mkoizumi <Masataro.koizumi@gmail.com>
2019-09-05 23:23:07 -0400

    Update views.py

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from account import forms
 from django.views.generic import CreateView
 
-# Create your views here.
+
 class Register(CreateView):
     form_class = forms.UserCreationForm
     success_url = reverse_lazy('login')  # sign up then it will take user to login.html
